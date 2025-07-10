ae5e71dce3274ccb63ec29309ad80ec78718f59a
綿田千愛 <chiyorinopc@watadasenainoMacBook-Air.local>
2025-07-10 20:16:13 +0900

    最初のコミット

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,19 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@GetMapping("/keisan")
+	public String hello(){
+		return "ようこそ仮想空間へ";
+	}
+}
